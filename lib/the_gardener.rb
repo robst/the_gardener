dc9535102b11c1e5eb088be78c50d7bd972c4ba1
robst <robertst81@gmail.com>
2016-02-12 21:05:14 +0100

    moved seed migration class into seperate file, moved logic into module and refactored small amount of code

--- lib/the_gardener.rb
+++ lib/the_gardener.rb
@@ -1,4 +1,5 @@
 require "the_gardener/version"
+require "the_gardener/seed_migration"
 
 module TheGardener
   if defined?(Rails)
@@ -10,18 +11,43 @@ module TheGardener
       end
     end
   end
-  class SeedMigration < ActiveRecord::Base
-  end
 
   def self.versioned? version
-    SeedMigration.where('version=?', version).any?
+    SeedMigration.version(version).any?
   end
 
-  def self.versionize version
-    SeedMigration.create(version: version)
+  def self.versionize! version
+    SeedMigration.generate(version)
+  end
+
+  def self.load_seeds!
+    Dir.open(seed_directory).sort.each do |seed_file|
+      next unless File.file? File.join(seed_directory,seed_file)
+      next if versioned?(filename_version(seed_file))
+      puts "loading seed: #{seed_file}"
+      begin
+        ActiveRecord::Base.transaction do
+          load File.join(seed_directory,seed_file)
+          versionize! filename_version(seed_file)
+        end
+      rescue => e 
+        puts "#{'-'*30}\nError on: #{File.join(seed_directory,seed_file)}"
+        puts "message: #{e.message}\n#{'-'*30}"
+        puts "seeding stopped" 
+        raise ActiveRecord::Rollback
+      end
+
+    end
   end
 
   def self.filename_version filename
     filename.split('_').first
   end
+
+  private
+
+  def self.seed_directory
+    File.join('db','seeds')
+  end
+
 end
